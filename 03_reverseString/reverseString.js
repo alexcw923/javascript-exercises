3b8f637a6f0be29e430fb6a734824c77d26617bf
Alex <carriealex2001@gmail.com>
2023-03-25 13:24:03 -0700

    Finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let res = "";
+    let arr = string.split("");
+    for (let i = arr.length-1; i >= 0; i--) {
+        res+=arr[i];
+    }
+    return res
 };
 
 // Do not edit below this line
